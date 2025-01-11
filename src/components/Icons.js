fb74659d21b9596b28d9d288323499b4c6ca04ed
sagarpoudel638 <sagarpoudel638@gmail.com>
2025-01-11 21:12:10 +1100

    style changes

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -2,13 +2,13 @@ import React from 'react';
 import icons from '../assets/icons'; // Import the icon map
 
 export default function Icons({ iconName, alt, className }) {
-  const iconSrc = icons[iconName]; // Dynamically select the image
+  const iconSrc = icons[iconName]; 
 
-  console.log('Icon source:', iconSrc); // Debugging: Ensure the iconSrc is not undefined
+  console.log('Icon source:', iconSrc); 
 
   if (!iconSrc) {
     console.error(`Icon '${iconName}' not found in the icons map.`);
-    return null; // Optionally return null if the icon isn't found
+    return null; 
   }
 
   return (
